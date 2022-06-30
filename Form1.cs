40219e6a5348d788be12e01ffcd01a3060c8320a
RedbirdXVO <63523784+RedbirdXVO@users.noreply.github.com>
2022-06-29 20:17:53 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace GroupProjectCSharpSummer22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
